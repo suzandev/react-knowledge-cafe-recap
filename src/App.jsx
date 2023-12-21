4e0fd31e65ee0c840424802f141bbddae8afbf39
suzandev <dev.suzanchandra@gmail.com>
2023-12-21 15:44:35 +0600

    project setup with daisyUI and tailwindCss. and create some fake data with json format

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Blogs from "./Components/Blogs/Blogs";
+import Header from "./Components/Header/Header";
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <Header />
+      <Blogs />
+    </div>
+  );
+}
+
+export default App;
